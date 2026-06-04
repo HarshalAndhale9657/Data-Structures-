867598aa7b209129400cacc83409b0a7a8555061
HarshalAndhale9657 <harshalandhale9@gmail.com>
2026-06-04 15:06:33 +0530

    coin change

---
+++ Dynamic Programming/coinChange.java
@@ -0,0 +1,39 @@
+public class coinChange{
+
+    public static int coinChange(int coins[], int sum){
+        int n = coins.length;
+        int dp[][] = new int [n+1][sum+1];
+
+        // initialize sum = 0 case
+        // i -> coins, j -> sum
+        for(int i = 0; i<n+1 ; i++){
+            dp[i][0] = 1;
+        }
+        for(int j = 1; j<sum+1 ; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1 ; i<n+1; i++){
+            for(int j = 1; j< sum+1; j++){
+                if(coins[i-1] <= j){
+                    dp[i][j] =   dp[i][j-coins[i-1]] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][sum];
+    
+
+    }
+
+    public static void main(String[] args) {
+        int coins [] = {1,2,3};
+        int sum = 4; // ans = 4
+
+        int ans = coinChange(coins, sum);
+        System.out.println(ans);
+    }
+    
+}
